38595d1f2e446529994b55036602c95fcdfe9dbe
vinhub100 <vinmail17@gmail.com>
2022-06-30 10:57:18 +0530

    new spa

---
+++ precache-manifest.75fd029b10feaaaa791769780fb9b32c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "262b5494b5adb2041598",
+    "url": "/static/css/main.d88bfcf9.chunk.css"
+  },
+  {
+    "revision": "262b5494b5adb2041598",
+    "url": "/static/js/main.262b5494.chunk.js"
+  },
+  {
+    "revision": "30e9960dcfd0682496ad",
+    "url": "/static/js/1.30e9960d.chunk.js"
+  },
+  {
+    "revision": "ce0583d8da3f767d9c90",
+    "url": "/static/css/2.a87da336.chunk.css"
+  },
+  {
+    "revision": "ce0583d8da3f767d9c90",
+    "url": "/static/js/2.ce0583d8.chunk.js"
+  },
+  {
+    "revision": "38d81f54dccc8e7b761d",
+    "url": "/static/js/runtime~main.38d81f54.js"
+  },
+  {
+    "revision": "18507283ad62d9875daf5ec1309e1a87",
+    "url": "/static/media/quickchart.18507283.webp"
+  },
+  {
+    "revision": "55dba61e459ffca479937a30c17cf88d",
+    "url": "/static/media/gde.55dba61e.webp"
+  },
+  {
+    "revision": "cf5655d8d19ef96e0a62361fe9b9f4de",
+    "url": "/static/media/tm19_1.cf5655d8.webp"
+  },
+  {
+    "revision": "554f6dfeb84abacb25e86e496e2c2fda",
+    "url": "/index.html"
+  }
+];
